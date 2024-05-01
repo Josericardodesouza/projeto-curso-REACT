0f4a2225a0131c743eedbfc767d7aea7daf5aad7
Josericardodesouza <souzaricardo.professionaldev@gmail.com>
2024-05-01 15:29:19 -0300

    update

--- costs/src/App.js
+++ costs/src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <ul>
+        <li>Home</li>
+        <li>Contato</li>
+      </ul>
+
+      <Routes>
+        <Route exact path='/' element = {<Home />}>
+      </Route>
+
+      <Route exact path = '/company' element = {<Company />}></Route>
+
+      <Route exact path = '/contact' element = {<Contact />}></Route>
+
+      <Route exact path = '/newproject' element = {<NewProject />}></Route>
+
+
+
+
+
+
+      <p>Footer</p>
+    </Routes>
+    </Router>
   );
 }
 
