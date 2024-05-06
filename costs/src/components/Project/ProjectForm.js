13eb57fbbc68d7bca7ea72b83b5c3d9ac23fe194
Josericardodesouza <souzaricardo.professionaldev@gmail.com>
2024-05-06 15:24:02 -0300

    aula 23

--- costs/src/components/Project/ProjectForm.js
+++ costs/src/components/Project/ProjectForm.js
@@ -1,9 +1,33 @@
+import { useState, useEffect } from 'react'
+
 import Select from '../form/Select'
 import SubmitButton from '../form/SubmitButton'
 import Input from '../form/input'
 import styles from './projectForm.module.css'
 
+
+
 function ProjectForm({btnText}) {
+
+    const [categories, setCategories] = useState([]) // seta para o array vazio esperando o que vai vir da API
+
+   useEffect (() => { // evita que o react fique fazendo milhares de requests, agora ele espera um valor inicial
+    fetch("http://localhost:5000/categories", {
+        method: "GET", 
+        headers: {
+            'Content-Type': 'application/json'
+        } // com o fetch fez o request para a url categories
+
+    })
+    .then((resp) => resp.json()) // transforma os dados da resposta em JSON
+    .then((data) => {
+        setCategories(data) // pega os dados em JSON e coloca no hook de set categories
+    })
+    // encadeia outros processos que deve fazer com a resposta da API
+    .catch((err) => console.log(err)) // imprime um erro que pode acontecer no request
+   }, []) // valor inicial, as options vazias
+
+
     return (
         <form className={styles.form}>
             <Input 
@@ -19,7 +43,9 @@ function ProjectForm({btnText}) {
              placeholder= 'Insira o orçamento total'
             />
             <div>
-             <Select name='category_id' text = 'Selecione a categoria'/>
+             <Select name='category_id' text = 'Selecione a categoria'
+             options={categories}
+             />
             </div>
             <div>
                 <SubmitButton text={btnText}  />
--- costs/src/components/form/Select.js
+++ costs/src/components/form/Select.js
@@ -6,6 +6,23 @@ function Select ({ text, name, options, handleOnChange, value}) {
             <label htmlFor={name}>{text}:</label>
             <select name={name} id={name} >
                 <option>Selecione uma opção</option>
+                {options.map((option) => (
+                  
+                    <option value={option.id} key = {option.id}>{option.name}
+
+                    // a option de id é pra connseguir lá no backend  como vai monitorar a categoria de cada projeto
+
+                    // a key diz que é uma option 
+                    única
+                    
+                    //option name vai ser o nome dela para o usuário poder selecionar
+               
+
+
+                    </option>
+
+                ))}
+
             </select>
         </div>
     )
